bdfb761f47be60c88222b31462002d98aca60f40
Miracle Billy <uniquemiraculous@gmail.com>
2024-03-28 14:48:47 +0100

    Subscribe button error updated

--- src/components/SubscribeButton.jsx
+++ src/components/SubscribeButton.jsx
@@ -1,3 +1,4 @@
+import { toast } from 'react-hot-toast'
 import useCurrentProfile from "../hooks/useCurrentProfile";
 import Button from "../styles/Button";
 import { toggleSubscribeUser } from "../utils/supabase";
@@ -6,9 +7,19 @@ function SubscribeButton({ subscribedToId }) {
   const profile = useCurrentProfile();
   const isSubscribed = profile?.subscriptions.includes(subscribedToId);
 
+  const handleClick = () => {
+    if(!profile){
+      toast.error("Sign in to Subscribe to a Channel")
+      
+    } else {
+      toggleSubscribeUser(profile, subscribedToId)
+    }
+  }
+
 
   return (
-  <Button onClick={() => toggleSubscribeUser(profile, subscribedToId)} grey={isSubscribed}>
+    
+  <Button onClick={handleClick} grey={isSubscribed}>
     {isSubscribed ? "Subscribed" : "Subscribe"}
   </Button>
   )
